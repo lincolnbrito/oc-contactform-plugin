8dd69d66f52c2e2bf5de432b34be58cdb37510ed
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-08 15:20:52 -0300

    send email

--- components/ContactForm.php
+++ components/ContactForm.php
@@ -1,6 +1,10 @@
 <?php namespace LincolnBrito\ContactForm\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use October\Rain\Exception\ValidationException;
 
 class ContactForm extends ComponentBase
 {
@@ -11,4 +15,38 @@ class ContactForm extends ComponentBase
             'description' => 'A simple contact form'
         ];
     }
+
+    public function onSend()
+    {
+        $data = Input::only([
+            'first_name', 'last_name', 'email', 'content'
+        ]);
+
+        $this->validate($data);
+
+        $receiver = 'admin@admin.tld';
+
+        Mail::send('lincolnbrito.contactform::contact', $data, function($message) use($receiver, $data) {
+            $message->to($receiver);
+            $message->replyTo($data['email']);
+        });
+    }
+
+    public function validate(array $data) 
+    {
+        $rules = [
+            'first_name' => 'required|min:3|max:255',
+            'last_name' => 'required|min:3|max:255',
+            'email' => 'required|email',
+            'content' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+    }
+
+
 }
